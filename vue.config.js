632933263a4b83fe558a6669a81b7508df831f4a
herochan <16961941@qq.com>
2019-05-05 14:47:18 +0800

    提交

--- vue.config.js
+++ vue.config.js
@@ -11,8 +11,8 @@ module.exports = {
                 }
             },
             '/test': {
-                // target: 'http://192.168.0.106:8085',
-                target: 'http://39.107.89.154:8000/commonservice-system',
+                target: 'http://192.168.0.106:8085',
+                // target: 'http://39.107.89.154:8000/commonservice-system',
                 changeOrigin: true,
                 ws: true,
                 pathRewrite: {
